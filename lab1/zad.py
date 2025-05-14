cd07c0d06019acfb3c5fe9c4fcc62686c855b622
MichalPlucinski1 <mic.plu02@gmail.com>
2025-05-14 17:02:38 +0200

    lab1

---
+++ lab1/zad.py
@@ -0,0 +1,8 @@
+# Cel	ćwiczenia	laboratoryjnego:	zapoznanie	się	z	tematyką	generatorów	ciągów	
+# pseudolosowych,	analiza	własności	jakie	powinny	posiadać	takie	ciągi,	implementacja	
+# generatora	BBS	oraz	4	testów	FIPS	140-2	(pojedynczych	bitów,	długiej	serii,	serii	oraz	
+# pokerowego).
+
+
+# 1. . Przeanalizuj	na	podstawie	podanej	literatury	jakie	własności	powinien	posiadać	
+# ciąg	pseudolosowy.	
